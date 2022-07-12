3d3a28e79cde5ab0201f741f8b7431589e77876b
David Mellen <d.mellen@remind.de>
2022-07-12 15:21:19 +0200

    Add image_gallery CType
    RECONTEXT-19

---
+++ Configuration/TCA/Overrides/205_content_element_image_gallery.php
@@ -0,0 +1,44 @@
+<?php
+
+defined('TYPO3_MODE') || die;
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'LLL:EXT:rmnd_content/Resources/Private/Language/locallang.xlf:content_element.image_gallery.title',
+        'image_gallery',
+        'content-gallery',
+    ],
+    'header',
+    'after'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    '*',
+    'FILE:EXT:rmnd_content/Configuration/FlexForms/ImageGallery.xml',
+    'image_gallery'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['image_gallery'] = [
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            --palette--;;headers,
+            pi_flexform,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.images,
+            image,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;;frames,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+    ',
+];
