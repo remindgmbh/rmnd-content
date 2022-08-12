8748a6c4d1a52541cd715715867df2cfa1bcc176
David Mellen <d.mellen@remind.de>
2022-08-12 07:26:11 +0200

    Apply configuration files rules and best practices

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,27 +1,33 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:rmnd_content/Configuration/TSConfig/Page/*.tsconfig"');
+declare(strict_types=1);
 
-/*******************************************************************************
- * Overrides                                                                   *
- ******************************************************************************/
+defined('TYPO3') or die();
 
-$GLOBALS
-    ['TYPO3_CONF_VARS']
-    ['SC_OPTIONS']
-    ['cms/layout/class.tx_cms_layout.php']
-    ['tt_content_drawItem']
-    ['items'] = \Remind\Typo3Content\Hooks\PageLayoutView\ItemsPreviewRenderer::class;
-
-/*******************************************************************************
- * Icon registry                                                               *
- ******************************************************************************/
-
-/* @var $iconRegistry \TYPO3\CMS\Core\Imaging\IconRegistry */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-
-$iconRegistry->registerIcon(
-    'content-footer',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+(function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:rmnd_content/Configuration/TSConfig/Page/*.tsconfig"');
+    
+    /*******************************************************************************
+     * Overrides                                                                   *
+     ******************************************************************************/
+    
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SC_OPTIONS']
+        ['cms/layout/class.tx_cms_layout.php']
+        ['tt_content_drawItem']
+        ['items'] = \Remind\Typo3Content\Hooks\PageLayoutView\ItemsPreviewRenderer::class;
+    
+    /*******************************************************************************
+     * Icon registry                                                               *
+     ******************************************************************************/
+    
+    /* @var $iconRegistry \TYPO3\CMS\Core\Imaging\IconRegistry */
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    
+    $iconRegistry->registerIcon(
+        'content-footer',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class ,
     ['source' => 'EXT:rmnd_content/Resources/Public/Icons/content-footer.svg']
-);
+    );
+})();
--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,8 @@
 <?php
 
-defined('TYPO3_MODE') || die;
+declare(strict_types=1);
+
+defined('TYPO3') or die();
 
 (function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('rmnd_content_items');
